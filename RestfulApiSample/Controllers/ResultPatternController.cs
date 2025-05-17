b7d7946ce4ea65aae2f9418defa9d3a664e05fd3
Parham Darvishi <darvishiparham14@gmail.com>
2025-05-17 16:23:59 +0330

    change on Result Pattern

--- RestfulApiSample/Controllers/ResultPatternController.cs
+++ RestfulApiSample/Controllers/ResultPatternController.cs
@@ -10,15 +10,15 @@ public class ResultPatternController : Controller
         return View();
     }
 
-    public async Task<IActionResult> RegisterUser(string email, string password)
-    {
-        if (string.IsNullOrWhiteSpace(email))
-            return Result.Failure(Error.InvalidEmail);
+    //public async Task<IActionResult> RegisterUser(string email, string password)
+    //{
+    //    if (string.IsNullOrWhiteSpace(email))
+    //        return Result.Failure(Error.);
 
-        if (string.IsNullOrWhiteSpace(password))
-            return Result.Failure(Error.InvalidPassword);
+    //    if (string.IsNullOrWhiteSpace(password))
+    //        return Result.Failure(Error.InvalidPassword);
 
         
-        return Result.Success();
-    }
+    //    return Result.Success();
+    //}
 }
