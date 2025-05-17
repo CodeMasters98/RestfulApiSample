b74fc942769169f7703cb396a4e2630165bf09be
Parham Darvishi <darvishiparham14@gmail.com>
2025-05-17 16:06:49 +0330

    Change

---
+++ RestfulApiSample/Controllers/ResultPatternController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RestfulApiSample.Models;
+
+namespace RestfulApiSample.Controllers;
+
+public class ResultPatternController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public async Task<IActionResult> RegisterUser(string email, string password)
+    {
+        if (string.IsNullOrWhiteSpace(email))
+            return Result.Failure(Error.InvalidEmail);
+
+        if (string.IsNullOrWhiteSpace(password))
+            return Result.Failure(Error.InvalidPassword);
+
+        
+        return Result.Success();
+    }
+}
